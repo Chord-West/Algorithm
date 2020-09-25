303535150ad4298cda3876f4732215f7eec65d4c
KimHyunSeo <gustj2005@naver.com>
2020-09-25 22:02:36 +0900

    sovled MaximumSubarray

--- Lecture/Lecture/Lecture/MaxmumSubarray.cpp
+++ Lecture/Lecture/Lecture/MaxmumSubarray.cpp
@@ -8,12 +8,22 @@ using namespace std;
 
 int solution(vector<int> param0) {
     int answer = 0;
-
+    int max = -21470000;
+    int sum = 0;
+    for (int i = 0; i < param0.size(); i++) {
+        sum += param0[i];
+        if (sum < param0[i])
+            sum = param0[i];
+        if (sum > max) {
+            max = sum;
+        }
+    }
+    answer = max;
     return answer;
 }
 
 
 int main() {
-    vector<int> arr = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+    vector<int> arr = { -72,94,-4 };
     cout << solution(arr);
 }
---
+++ Lecture/Lecture/Lecture/ex.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int number = 8;
+vector<int> sorted(8);
+
+void merge(int a[], int m, int middle, int n) {
+	int i = m;// 2개중 첫번째
+	int j = middle + 1;// 2개중 두번째
+	int k = m; // 합친 배열의 첫번째
+	// 작은 순서대로 배열에 삽입
+	while (i <= middle && j <= n) {
+		//작은 값을 K에 넣어줌
+		if (a[i] <= a[j]) {
+			sorted[k] = a[i];
+			i++;
+		}
+		else {
+			sorted[k] = a[j];
+			j++;
+		}
+		k++;
+	}
+	if (i > middle) // i 가 먼저 배열에 도착했을 경우
+	{
+		for (int t = j; t <= n; t++) {
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	else {
+		for (int t = i; t <= middle; t++) {
+			sorted[k] = a[t];
+			k++;
+		}
+	}
+	//정렬된 배열을 삽입
+	for (int t = m; t <= n; t++) {
+		a[t] = sorted[t];
+	}
+}
+void mergesort(int a[], int p, int r) {
+	if (p < r) {
+		int middle = (p + r) / 2;
+		mergesort(a, p, middle);
+		mergesort(a, middle + 1, r);
+		merge(a, p, middle, r);
+	}
+}
+
+
+int main() {
+	int array[8] = { 7,6,5,8,3,5,9,1 };
+	mergesort(array, 0, 7);
+	for (int i = 0; i < 8; i++) {
+		cout << array[i] << " ";
+	}
+}
