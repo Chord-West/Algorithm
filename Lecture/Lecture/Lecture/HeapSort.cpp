4d1ed1431432a5c194a3ffa860579c379ca2c4c6
KimHyunSeo <gustj2005@naver.com>
2020-10-18 23:02:02 +0900

    studied about quicksort

--- Lecture/Lecture/Lecture/HeapSort.cpp
+++ Lecture/Lecture/Lecture/HeapSort.cpp
@@ -3,45 +3,41 @@
 
 using namespace std;
 
-int number = 9;
 vector<int> heap = { 7,6,5,8,3,5,9,1,6 }; 
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	// 먼저 전체 트리 구조를 max heap 구조로 바꿈
-	for (int i = 1; i < number; i++) {
-		int child = i;
-		do {
-			int root = (child - 1) / 2;
-			if (heap[root] < heap[child]) {
+	for (int i = 1; i < heap.size(); i++) {
+		int c = i; // 자식
+		
+		while (c != 0) {
+			int root =(c - 1)/2 ;
+			if (heap[root] < heap[c]) {
 				int tmp = heap[root];
-				heap[root] = heap[child];
-				heap[child] = tmp;
+				heap[root] = heap[c];
+				heap[c] = tmp;
 			}
-			child = root;
-		} while (child != 0);
+			c = root;
+		}
 	}
-	// 크기를 줄여가며 반복적으로 힙을 구성 ( 정렬 )
-	for (int i = number - 1; i >= 0; i--) {
+	for (int i = heap.size() - 1; i >= 0;i--) {
 		int tmp = heap[0];
 		heap[0] = heap[i];
 		heap[i] = tmp;
 		int root = 0;
-		int child = 1;
-		do {
-			child = 2 * root + 1; //자식 중에 더 큰 값을 찾기
-			if (child < i - 1&&heap[child] < heap[child + 1] ) {
-				child++; 
-			}
-			//루트보다 자식이 더 크다면 교환
-			if (child < i&&heap[root] < heap[child] ) {
+		int c = 1;
+		while (c < i) {
+			c = 2 * root + 1;
+			if (c < i - 1 && heap[c] < heap[c + 1]) c++;
+			if (c < i && heap[root] < heap[c]) {
 				int tmp = heap[root];
-				heap[root] = heap[child];
-				heap[child] = tmp;
+				heap[root] = heap[c];
+				heap[c] = tmp;
 			}
-			root = child;
-		}while(child < i);
+			root = c;
+		}
 	}
-	for (int i = 0; i < number; i++)
+	for (int i = 0; i < heap.size(); i++) {
 		cout << heap[i] << " ";
+	}
 }
