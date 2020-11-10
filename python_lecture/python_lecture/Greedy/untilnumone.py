ed5c81a886c2f1514a6b4b4d904de7b9454df83e
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2020-11-10 22:13:03 +0900

    study greedy algorithm

---
+++ python_lecture/python_lecture/Greedy/untilnumone.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("input.txt","rt")
+
+n , k = map(int,input().split())
+count = 0
+
+while True:
+    target = (n//k)*k
+    count += n-target;
+    n=target;
+
+    if n<k:
+        break
+    count+=1
+    n//=k
+
+count+=n-1
+print(count)
