c48238b7774442e35aec722f791b30c255281c45
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-01-14 21:53:41 +0900

    solved 단어찾기, 교육과정설계

--- PythonAlgorithm/Python_AA/Python_AA/Section4/교육과정설계.py
+++ PythonAlgorithm/Python_AA/Python_AA/Section4/교육과정설계.py
@@ -1,15 +1,13 @@
-#import sys
-#sys.stdin = open("input.txt","rt")
-from collections import deque
+import sys
+sys.stdin = open("input.txt","rt")
 
 if __name__ == "__main__":
     s = [x for x in input()]
     n = int(input())
     ans=[]
-    dq=deque(s)
     for i in range(n):
         for x in input():
-            if any( x==s for s in dq):
+            if x in s:
                 ans.append(x)
         if ans==s:
             print("#{} YES".format(i+1))
