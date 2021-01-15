9a95ef74c70625362c7c5e7e0ab376d1634c7d6f
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-01-15 15:12:47 +0900

    solved 아나그램(딕서녀러 해쉬)

--- PythonAlgorithm/Python_AA/Python_AA/Section4/단어찾기.py
+++ PythonAlgorithm/Python_AA/Python_AA/Section4/단어찾기.py
@@ -1,10 +1,16 @@
-#import sys
-#sys.stdin = open("input.txt","rt")
+import sys
+sys.stdin = open("input.txt","rt")
 
 if __name__ =="__main__":
     n = int(input())
-    words=[ input() for _ in range(n)]
-    words2=[input() for _ in range(n-1)]
-    for x in words:
-        if x not in words2:
-            print(x)
+    p=dict()
+    for i in range(n):
+        word=input()
+        p[word]=1
+    for i in range(n-1):
+        word=input()
+        p[word]=0
+
+    for key,value in p.items():
+        if value==1:
+            print(key)
