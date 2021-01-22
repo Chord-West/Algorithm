9041c03636bb4f6f376f76df01f2680c433458a1
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-01-23 00:03:11 +0900

    solved 뮤직비디오

--- PythonAlgorithm/Python_AA/Python_AA/Section3/랜선자르기.py
+++ PythonAlgorithm/Python_AA/Python_AA/Section3/랜선자르기.py
@@ -1,23 +1,23 @@
 import sys
 sys.stdin = open("input.txt","rt")
 
+def Count(len):
+    cnt=0
+    for x in line:
+        cnt+=(x//len)
+    return cnt
+
 if __name__ =="__main__":
     k,n=map(int,input().split())
     line=[int(input()) for _ in range(k)]
-    g=sum(line)//n
-    ch=0
-    mid=g
-    lt,rt=0,g
-    max=0
+    line.sort()
+    res=0
+    lt,rt=0,line[k-1]
     while lt<=rt:
         mid=(lt+rt)//2
-        for x in line:
-            ch += x // mid
-        if ch<n:
-            rt=mid-1
-        elif ch>=n:
+        if Count(mid)>=n:
+            res=mid
             lt=mid+1
-            if mid>max:
-                max=mid
-        ch=0
-    print(max)
+        else:
+            rt=mid-1
+    print(res)
