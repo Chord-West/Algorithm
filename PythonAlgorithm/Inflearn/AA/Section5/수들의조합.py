4fe5e8fbfbce253aee8242a6f1db1f67bb8f04ad
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-01-26 18:52:53 +0900

    solved 수들의 조합 ( DFS )

---
+++ PythonAlgorithm/Inflearn/AA/Section5/수들의조합.py
@@ -0,0 +1,25 @@
+import sys
+#sys.stdin = open("input.txt","rt")
+
+def DFS(L):
+    global cnt
+    if L==k:
+        if sum(res)%m==0:
+            cnt+=1
+    else:
+        for i in range(len(num)):
+            if ch[i]==0 and res[L-1]<num[i]:
+                ch[i]=1
+                res[L]=num[i]
+                DFS(L+1)
+                ch[i]=0
+
+if __name__ =="__main__":
+    n,k= map(int,input().split())
+    num=list(map(int,input().split()))
+    ch=[0]*n
+    cnt=0
+    m=int(input())
+    res = [0] * m
+    DFS(0)
+    print(cnt)
