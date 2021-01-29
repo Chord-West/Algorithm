7fddc20642ddf366e1151a7695cef0b7c729ec5f
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-01-30 01:14:36 +0900

    solved 양팔저율( DFS )

---
+++ PythonAlgorithm/Inflearn/AA/Section6/양팔저울.py
@@ -0,0 +1,25 @@
+import sys
+#sys.stdin = open("input.txt","rt")
+
+def DFS(L,sum):
+    global total
+    if L==k:
+        if 0<sum and sum<=total:
+            ch[sum]=1
+    else:
+        DFS(L+1,sum+w[L])
+        DFS(L+1, sum)
+        DFS(L+1, sum-w[L])
+
+
+if __name__ == "__main__":
+    k= int(input())
+    w = list(map(int,input().split()))
+    total=sum(w)
+    ch=[0]*(total+1)
+    DFS(0,0)
+    cnt=0
+    for i in range(1,len(ch)):
+        if ch[i]==0:
+            cnt+=1
+    print(cnt)
