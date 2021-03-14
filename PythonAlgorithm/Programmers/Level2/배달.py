f8b4006fb87879792b280fdcc30fb6e5803a0739
KimHyeonSeo <50789483+Chord-West@users.noreply.github.com>
2021-03-14 16:42:11 +0900

    solved 2 problems

--- PythonAlgorithm/Programmers/Level2/배달.py
+++ PythonAlgorithm/Programmers/Level2/배달.py
@@ -1,25 +1,19 @@
+from collections import deque
+
+
 def solution(N, road, K):
-    answer = 0
+    nodes = dict()
+    dist = {i: float('inf') if i != 1 else 0 for i in range(1, N + 1)}
+    for v1, v2, d in road:
+        nodes[v1] = nodes.get(v1, []) + [(v2, d)]
+        nodes[v2] = nodes.get(v2, []) + [(v1, d)]
 
-    def dfs(L, S):
-        if S <= K:
-            print(L)
-            ch[L] = 1
-        if S > K:  # 배달 시간 넘어갈경우
-            return
-        else:
-            for x in graph[L]:
-                if ch[x[0]] == 0:
-                    dfs(x[0], S + x[1])  # x[0]은 마을번호 , x[1은 시간]
+    que = deque([1])
+    while que:
+        cur_node = que.popleft()
+        for next_node, d in nodes[cur_node]:
+            if dist[next_node] > dist[cur_node] + d:
+                dist[next_node] = dist[cur_node] + d
+                que.append(next_node)
 
-    graph = [[] for _ in range(N + 1)]
-    ch = [0] * (N + 1)
-    for x in road:
-        graph[x[0]].append([x[1], x[2]])
-        graph[x[1]].append([x[0], x[2]])
-    dfs(1, 0)
-    print(graph)
-    print(ch)
-    return answer
-
-solution(5,[[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]],3)
+    return len([True for x in dist.values() if x <= K])
