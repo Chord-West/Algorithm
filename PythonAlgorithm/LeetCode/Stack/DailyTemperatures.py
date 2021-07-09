1b8ce7cf99cc39a7e61553b2dad53613e8a2528e
unknown <gustj2005@naver.com>
2021-07-09 15:03:21 +0900

    solved 2 problems ( 스택 응용 )

---
+++ PythonAlgorithm/LeetCode/Stack/DailyTemperatures.py
@@ -0,0 +1,12 @@
+def dailyTemperatures(T):
+    ans=[0] * len(T)
+    stack=[]
+    for i, t in enumerate(T):
+        # 현재 온도가 스택 값보다 높다면 정답처리
+        while stack and t>T[stack[-1]]:
+            last = stack.pop()
+            ans[last] = i -last
+        stack.append(i)
+    return ans
+
+dailyTemperatures([73,74,75,71,69,72,76,73])
