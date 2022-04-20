3795e0b3d1a3cedf553f39b924cd0ea19c96cc78
chord.west <gustj2005@naver.com>
2022-04-20 22:10:14 +0900

    solved 1 problems

--- PythonAlgorithm/LeetCode/StringManipulation/validpalindrome.py
+++ PythonAlgorithm/LeetCode/StringManipulation/validpalindrome.py
@@ -1,11 +1,27 @@
+# 첫 풀이 90 ms	14.3 MB
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        word=''
-        for char in s:
-            if char.isalnum():
-                word+=char.lower()
-        return word==word[::-1]
+        s=s.lower()
+        word = ''
+        for x in s:
+            if x.isalnum():
+                word+=x
+        idx=0
+        while idx < len(word):
+            if word[idx] != word[-1-idx]:
+                return False
+            idx+=1
+        return True
 
+# 두번쨰 풀이
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = ''
+        for x in s.lower():
+            if x.isalnum():
+                word+=x            
+        return word==word[::-1]
+    
 # 정규표현식 이용
 import re
 class Solution:
