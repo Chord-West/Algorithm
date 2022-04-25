e7111cc05c1d204371fa30cf330ed02796bc9782
chord.west <gustj2005@naver.com>
2022-04-25 22:19:17 +0900

    solved 1 problems

--- PythonAlgorithm/LeetCode/StringManipulation/GroupAnagrams.py
+++ PythonAlgorithm/LeetCode/StringManipulation/GroupAnagrams.py
@@ -1,27 +1,13 @@
-# 내 방법 Runtime : 100ms, Memory : 17.2 MB
+# 159 ms	17.2 MB
 
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        p=dict()
-        answer=[]
+        p = defaultdict(list)
+        result = []
         for word in strs:
-            p[''.join(sorted(word))]=[]
-        for word in strs:
-             p[''.join(sorted(word))].append(word)
+            s = ''.join(sorted(list(map(str,word))))
+            p[s].append(word) 
         for k,v in p.items():
-            answer.append(v)
-        return answer
-
-#정렬하여 딕셔너리에 추가 ( defaultdict 활용 ) : Runtime : 92ms, Memory : 17.1MB
-
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        p=dict()
-        answer=[]
-        for word in strs:
-            p[''.join(sorted(word))]=[]
-        for word in strs:
-             p[''.join(sorted(word))].append(word)
-        for k,v in p.items():
-            answer.append(v)
-        return answer
+            result.append(v)
+        return result
