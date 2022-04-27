4f86d21b70e54c3fceabef7e9785ee3d524d94a7
chord.west <gustj2005@naver.com>
2022-04-27 22:57:47 +0900

    solved 1 problems

--- PythonAlgorithm/LeetCode/StringManipulation/LongestPalindromicSubstring.py
+++ PythonAlgorithm/LeetCode/StringManipulation/LongestPalindromicSubstring.py
@@ -1,16 +1,15 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        # 팰린드롬 판별 및 투 포인터 확장
-        def expand(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left + 1:right]
+#	350 ms	13.9 MB
+def longestPalindrome(s):   
+    def expand(left,right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return s[left+1:right]
+    if len(s)<2 or s == s[::-1]:
+        return s
+    result = ''
+    for i in range(len(s)-1):
+        result = max(result,expand(i,i+1),expand(i,i+2),key= len)          
+    return result
 
-        if len(s) < 2 or s == s[::-1]:
-            return s
-        result = ''
-        for i in range(len(s) - 1):
-            result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
-
-        return result
+longestPalindrome("babad")
