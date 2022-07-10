484ba2e67dc473ccdf98d9c3d9576a86cbd43ea1
Chord-West <gustj2005@naver.com>
2022-07-10 20:44:23 +0900

    solved 1 problems

--- PythonAlgorithm/Programmers/Level1/숫자문자열과영단어.py
+++ PythonAlgorithm/Programmers/Level1/숫자문자열과영단어.py
@@ -1,3 +1,4 @@
+# 내풀이
 def solution(s):
     answer = ''
     p = {'zero':'0','one':'1','two':'2','three':'3','four':'4',
@@ -12,3 +13,11 @@ def solution(s):
                 answer+=p[tmp]
                 tmp=''
     return int(answer)
+# 다른 사람 풀이
+def solution(s):
+    answer = ''
+    p = {'zero':'0','one':'1','two':'2','three':'3','four':'4',
+         'five':'5','six':'6','seven':'7','eight':'8','nine':'9' }
+    for key,value in p.items():
+        s = s.replace(key,value)
+    return int(s)
