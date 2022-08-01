f1bf12e55545b9f012a44a600d73ebf85ca3ad53
Chord-West <gustj2005@naver.com>
2022-08-01 23:56:03 +0900

    solved 1 problems

---
+++ PythonAlgorithm/Programmers/Level2/괄호변환.py
@@ -0,0 +1,41 @@
+
+def solution(p):
+    if p=='':
+        return p
+    u,v = devide(p)
+    reverse = {'(':')',')':'('}
+
+    if validate(u):
+        return u + solution(v)
+    else:
+        tmp = "("
+        for x in u[1:-1]:
+            tmp+=reverse[x]
+        tmp+=")"
+        return solution(v)
+
+def devide(p):
+    lb,rb = 0,0
+    for i,v in enumerate(p):
+        if v == '(':
+            lb+=1
+        else:
+            rb+=1
+        if lb>0 and lb==rb:
+            return p[:i+1],p[i+1:]
+
+def validate(w):
+    stack=[]
+    for brace in w:
+        if brace =='(':
+            stack.append(brace)
+        elif brace ==')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(brace)
+    return True if len(stack)==0 else False
+
+solution("(()())()")
+
+
