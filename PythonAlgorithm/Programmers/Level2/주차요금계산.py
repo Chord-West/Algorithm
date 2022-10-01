50aa122c9f23c9245c82a4f4d6ee4c7906027de8
Chord-West <gustj2005@naver.com>
2022-10-02 00:25:01 +0900

    solved 1 problem

--- PythonAlgorithm/Programmers/Level2/주차요금계산.py
+++ PythonAlgorithm/Programmers/Level2/주차요금계산.py
@@ -1,18 +1,26 @@
 from collections import defaultdict
-
-
+import math
 def solution(fees, records):
     answer = []
-    parking = defaultdict(int)
-    # 시간, 기본요금, 시간, 시간당 요금
-    for fee in fees:
-        print(fee)
+    park = defaultdict(list)
+    last = 60 * 23 + 59
     for record in records:
         time, num, info = record.split()
         h, m = map(int, time.split(":"))
         MM = 60 * h + m
-        if info == 'IN':
-            parking()
-        print(MM)
+        park[num].append(MM)
+
+    for k, v in park.items():
+        if len(v) % 2 == 1:
+            park[k].append(last)
+
+    for x in sorted(park):
+        t_sum = 0
+        for i in range(0, len(park[x]), 2):
+            t_sum += park[x][i + 1] - park[x][i]
+        if t_sum <= fees[0]:
+            answer.append(fees[1])
+        else:
+            answer.append(fees[1] + math.ceil((t_sum - fees[0]) / fees[2]) * fees[3])
 
     return answer
